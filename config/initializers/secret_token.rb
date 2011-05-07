ac0adcc5e46a7aaa115ce680e825fa52923e0f07
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2011-05-06 22:25:25 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LicaoApp::Application.config.secret_token = 'eb4dd4bd2f8079bda0e526984bf092aee679dbd8c16d04346dcb7c7773796acb8f9d3249778c7739a67850bb7e77bf8df6623a4d4596db609da3791e87e9ddff'
